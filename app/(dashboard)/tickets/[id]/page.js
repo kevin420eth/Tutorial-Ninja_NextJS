c9e6a38dc43976a160b908e0d69611854226b931
Kevin Mars <kevin.eth.420@gmail.com>
2023-09-30 14:52:41 +0800

    lesson 34

--- app/(dashboard)/tickets/[id]/page.js
+++ app/(dashboard)/tickets/[id]/page.js
@@ -1,44 +1,53 @@
-import React from 'react'
-import { Suspense } from "react";
-import Loading from '@/app/(dashboard)/loading'
-import TicketDetails from './TicketDetails';
-
-export async function generateMetadata({ params }) {
-    const id = params.id
-    const res = await fetch(`http://localhost:4000/tickets/${id}`)
-    const ticket = await res.json()
-
-    return {
-        title: `Dojo Helpdesk | ${ticket.title}`
-    }
-}
+import { notFound } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
 export const dynamicParams = true
 
-export async function generateStaticParams() {
-    const res = await fetch('http://localhost:4000/tickets')
+export async function generateMetadata({ params }) {
+    const supabase = createServerComponentClient({ cookies })
 
-    const tickets = await res.json()
+    const { data: ticket } = await supabase.from('Tickets')
+        .select()
+        .eq('id', params.id)
+        .single()
 
-    return tickets.map((ticket) => {
-        return (
-            {
-                id: ticket.id
-            }
-        )
-    })
+    return {
+        title: `Dojo Helpdesk | ${ticket?.title || 'Ticket not Found'}`
+    }
 }
 
+async function getTicket(id) {
+    const supabase = createServerComponentClient({ cookies })
 
-export default function Ticket({ params }) {
+    const { data } = await supabase.from('Tickets')
+        .select()
+        .eq('id', id)
+        .single()
+
+    if (!data) {
+        notFound()
+    }
+
+    return data
+}
+
+export default async function TicketDetails({ params }) {
+    const ticket = await getTicket(params.id)
+
     return (
         <main>
             <nav>
                 <h2>Ticket Details</h2>
             </nav>
-            <Suspense fallback={<Loading />}>
-                <TicketDetails ticket={params} />
-            </Suspense>
+            <div className="card">
+                <h3>{ticket.title}</h3>
+                <small>Created by {ticket.user_email}</small>
+                <p>{ticket.body}</p>
+                <div className={`pill ${ticket.priority}`}>
+                    {ticket.priority} priority
+                </div>
+            </div>
         </main>
     )
 }
